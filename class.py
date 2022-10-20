05e2f2449fd4485a4a01cf737fd4c38cee0d8027
MTeeeee <mete.ardic@gmail.com>
2022-10-20 15:01:05 +0200

    Update class.py

--- class.py
+++ class.py
@@ -13,34 +13,34 @@ class Papierkorb:
     fremdkoerper: int = 0 
     umgedreht: bool = False
 
-    def add_paper(self, fuellstand, leer, maximal_voll, umgedreht):
-        if maximal_voll == False and umgedreht = False:
-            fuellstand +=
-            if leer == True:
-                leer = False
+    def add_paper(self):
+        if self.maximal_voll == False and self.umgedreht == False:
+            self.fuellstand += 1
+            if self.leer == True:
+                self.leer = False
 
-            if fuellstand == 100:
-                maximal_voll = True
-        elif maximal_voll == True and umgedreht = False:
+            if self.fuellstand == 100:
+                self.maximal_voll = True
+        elif self.maximal_voll == True and self.umgedreht == False:
             print("Papierkorb ist voll! Bitte leeren.")
 
-    def papierkorb_leeren(self, fuellstand, leer, maximal_voll, umgedreht):
-        if maximal_voll == True and umgedreht = False:
-            maximal_voll = False
-            leer = True
-            fuellstand = 0
-        elif maximal_voll == False and umgedreht = False:
+    def papierkorb_leeren(self):
+        if self.maximal_voll == True and self.umgedreht == False:
+            self.maximal_voll = False
+            self.leer = True
+            self.fuellstand = 0
+        elif self.maximal_voll == False and self.umgedreht == False:
             print("Der Papierkorb ist bereits leer.")
 
-    def papierkorb_drehen(self, leer, umgedreht):
-        if leer == True:
-            umgedreht = True
+    def papierkorb_drehen(self):
+        if self.leer == True:
+            self.umgedreht = True
             print("Der Papierkorb ist jetzt umgedreht.")
         else:
             print("Der Papierkorb ist nicht leer, bitte erst leeren!")
 
-    def trommel_solo(self, umgedreht):
-        if umgedreht == True:
+    def trommel_solo(self):
+        if self.umgedreht == True:
             print("""
             TROMMELSOLOOOO!!!!
             BUM BUM BUMBUMBUM
